7097c623bac54f536123f851a203d80b48c685dd
AniketSharma24 <sharmaaniket2411@gmail.com>
2023-03-30 17:52:19 +0530

    Added restaurant cards and enhanced the UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,43 +1,44 @@
-import RestaurantCard from "./RestaurantCard";
-import { API_URL, RESTAURANT_LIST } from "../constants/constant";
-import { useEffect, useState } from "react";
+import RestaurantCard from './RestaurantCard';
+import { API_URL, RESTAURANT_LIST } from '../constants/constant';
+import { useEffect, useState } from 'react';
+import notFound from '../../assets/images/notFound.svg';
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 
 const searchRestaurants = (searchedText, restaurants) => {
-  console.log(restaurants, searchedText);
   return restaurants.filter((restaurant) =>
-    restaurant.data?.data?.name
-      ?.toLowerCase()
-      .includes(searchedText.toLowerCase())
+    restaurant.data?.name?.toLowerCase().includes(searchedText.toLowerCase())
   );
 };
 
 const BodyComponent = () => {
-  const [searchedText, setSearchedText] = useState("");
+  const [searchedText, setSearchedText] = useState('');
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [allRestaurants, setAllRestaurants] = useState([]);
 
   useEffect(() => {
     // Fake api
     getAllRestaurants();
+    console.log('useEffect() called');
   }, []);
 
   async function getAllRestaurants() {
     const response = await fetch(API_URL);
     const data = await response.json();
     const list = data?.data?.cards[2]?.data?.data?.cards;
-
     setFilteredRestaurants(list);
     setAllRestaurants(list);
   }
-
-  return (
+  console.log('render() called');
+  return allRestaurants.length === 0 ? (
+    <p className='search-text'>Searching...</p>
+  ) : (
     <div>
-      <p className="restaurant-count">
+      <p className='restaurant-count'>
         <span>{filteredRestaurants.length} restaurants</span>
         <span>
           <input
-            type="text"
-            placeholder="Search for restaurants..."
+            type='text'
+            placeholder='Search for restaurants...'
             value={searchedText}
             onChange={(e) => {
               setSearchedText(e.target.value);
@@ -54,16 +55,23 @@ const BodyComponent = () => {
           </button>
         </span>
       </p>
-      <div className="card-container">
-        {filteredRestaurants.map((restaurant) => {
-          return (
-            <RestaurantCard
-              {...restaurant.data?.data}
-              key={restaurant.data?.data?.id}
-            />
-          );
-        })}
-      </div>
+      {filteredRestaurants.length === 0 ? (
+        <div className='not-found-error'>
+          <img src={notFound} />
+          <p>
+            <ExclamationTriangleIcon className='hero-icon text-blue-500' />
+            No restaurant matches your search criteria
+          </p>
+        </div>
+      ) : (
+        <div className='card-container'>
+          {filteredRestaurants.map((restaurant) => {
+            return (
+              <RestaurantCard {...restaurant.data} key={restaurant.data?.id} />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,12 +1,48 @@
-import { IMG_CDN } from "../constants/constant";
+import { IMG_CDN } from '../constants/constant';
+import { StarIcon, ReceiptPercentIcon } from '@heroicons/react/24/solid';
 
-const RestaurantCard = ({ name, cloudinaryImageId, cuisines, avgRating }) => {
+const ratingClass = (rating) => {
+  if (rating < 4) {
+    return 'res-rating low';
+  } else if (rating >= 4) {
+    return 'res-rating high';
+  } else {
+    return 'res-rating';
+  }
+};
+
+const RestaurantCard = ({
+  name,
+  cloudinaryImageId,
+  cuisines,
+  avgRating,
+  costForTwoString,
+  slaString,
+  aggregatedDiscountInfo,
+}) => {
   return (
-    <div className="card">
+    <div className='card'>
       <img src={`${IMG_CDN + cloudinaryImageId}`} />
-      <h3>{name}</h3>
-      <p>{cuisines.join(", ")}</p>
-      <p>{avgRating} stars</p>
+      <p className='res-name'>{name}</p>
+      <p className='res-cuisine'>{cuisines.join(', ')}</p>
+      <p className='res-data'>
+        <span className={ratingClass(avgRating)}>
+          <StarIcon className='hero-icon text-blue-500' />
+          {avgRating}
+        </span>
+        <span className='seperator'>|</span>
+        <span className='res-sla'>{slaString}</span>
+        <span className='seperator'>|</span>
+        <span className='res-cost'>{costForTwoString}</span>
+      </p>
+      {aggregatedDiscountInfo?.shortDescriptionList[0] ? (
+        <p className='res-discount'>
+          <ReceiptPercentIcon className='hero-icon text-blue-500' />
+          {aggregatedDiscountInfo?.shortDescriptionList[0]?.meta}
+        </p>
+      ) : (
+        ''
+      )}
     </div>
   );
 };
